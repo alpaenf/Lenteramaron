c90a2a0222505a60dbf31388fecf26908e83ce62
alpaenf <alfaenf_@gmail.com>
2026-08-18 23:44:40 +0700

    ganti konsep

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,11 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/webp" href="/favicon.webp">
+        <link rel="icon" type="image/png" href="/favicon.png">
+        <link rel="shortcut icon" href="/favicon.ico">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
